77ceb003215d329fbe6e46da24237170502ffc11
alebelcor <856838+alebelcor@users.noreply.github.com>
2018-11-10 18:09:17 -0700

    Fix date

--- test/get-published-at.spec.js
+++ test/get-published-at.spec.js
@@ -5,7 +5,7 @@ import test from 'ava';
 import getPublishedAt from '../lib/get-published-at';
 
 test('it should return the date when the job was published', t => {
-  t.deepEqual('2016-12-15', getPublishedAt({posted: 1481846400000}));
+  t.deepEqual('2016-12-15', getPublishedAt({posted: 1481785200000}));
 });
 
 test('it should return null', t => {
